ff7dad2b52513727c7174a2859a9853e701d41da
siska <emmanuelsimisi@gmail.com>
2021-04-12 01:17:22 +0200

    Site moev mise a jour

--- Fiche-adhesion/index.php
+++ Fiche-adhesion/index.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require '../Php/Pdf.php';
 
 $pdf = new Pdf();
-$pdf->PdfPrint();
+$pdf->BuildPdf();
 
 
--- Php/Pdf.php
+++ Php/Pdf.php
@@ -22,20 +22,24 @@ class Pdf {
 
     }
 
-    /**
-     * @return \Twig\Environment
-     */
-    public function Bonjour()
+    public function Body()
     {
-        return $this->getTemplate();
+        $template =$this->getTemplate();
+        return $template->render('fiche.html.twig');
+    }
+    public function Footer()
+    {
+        $template=$this->getTemplate();
+        return $template->render('footer.html.twig');
     }
 
 
-    public function PdfPrint()
+    public function BuildPdf()
     {
-        $this->pdfLibrary->AddPage();
-        $this->pdfLibrary->AddPage();
-        $this->pdfLibrary->WriteHTML("<h1>Bonjour</h1>");
-        $this->pdfLibrary->Output();
+        $this->pdfLibrary->AddPage('','','','','',0,0,5,0,0,10);
+        $this->pdfLibrary->setAuthor('Emmanuel SIMISI');
+        $this->pdfLibrary->setHTMLFOOTER($this->Footer());
+        $this->pdfLibrary->WriteHTML($this->Body());
+        $this->pdfLibrary->Output('fiche-adhesion.pdf',I);
     }
 }
